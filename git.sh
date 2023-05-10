868fe5dd05729c7bbe949766c3cfccf39a6ff28f
Pasit Y <ezynook@gmail.com>
2023-05-10 14:35:38 +0700

    added script push automate at: 2023-05-10 14:35:38

--- git.sh
+++ git.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Enter your Message: "
+read msg
 git add .
-git commit -m "Update at: $(date +'%Y%m%d %H%M%S')"
+git commit -m "$msg at: $(date +'%Y-%m-%d %H:%M:%S')"
 git push origin main
